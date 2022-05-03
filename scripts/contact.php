1b1f916c64e4baf7f96bba2ae3d35793b8065e37
Toopla <alexis.grislin@outlook.fr>
2022-05-03 16:28:31 +0200

    First Commit

---
+++ scripts/contact.php
@@ -0,0 +1,32 @@
+<?php
+if(isset($_POST['envoyer']))
+{
+	if(!empty($_POST['nom']) AND !empty($_POST['mail']) AND !empty($_POST['message']))
+	{
+		$header="MIME-Version: 1.0\r\n";
+		$header.='From:"Groupe-Socafluid.fr"<stagiaire@socafluid.fr>'."\n";
+		$header.='Content-Type:text/html; charset="uft-8"'."\n";
+		$header.='Content-Transfer-Encoding: 8bit';
+
+		$message='
+		<html>
+			<body>
+				<div align="center">
+					<u>Nom de l\'expéditeur :</u>'.$_POST['nom'].'<br />
+					<u>Mail de l\'expéditeur :</u>'.$_POST['mail'].'<br />
+					<br />
+					'.nl2br($_POST['message']).'
+				</div>
+			</body>
+		</html>
+		';
+
+		mail("stagiaire@socafluid.fr", "CONTACT - Groupe-Socafluid.fr", $message, $header);
+		$msg="Votre message a bien été envoyé !";
+	}
+	else
+	{
+		$msg="Tous les champs doivent être complétés !";
+	}
+}
+?>
