53d0063cee5cc6e5709b6b8c3f8e6dcee4e7c3ca
Toopla <alexis.grislin@outlook.fr>
2022-05-03 16:49:07 +0200

    Ajout dans contact.php

--- scripts/contact.php
+++ scripts/contact.php
@@ -1,8 +1,6 @@
 <?php
-if(isset($_POST['envoyer']))
-{
-	if(!empty($_POST['nom']) AND !empty($_POST['mail']) AND !empty($_POST['message']))
-	{
+if(isset($_POST['envoyer'])) {
+	if(!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['societe']) AND !empty($_POST['telephone']) !empty($_POST['mail']) AND !empty($_POST['message'])) {
 		$header="MIME-Version: 1.0\r\n";
 		$header.='From:"Groupe-Socafluid.fr"<stagiaire@socafluid.fr>'."\n";
 		$header.='Content-Type:text/html; charset="uft-8"'."\n";
@@ -13,7 +11,11 @@ if(isset($_POST['envoyer']))
 			<body>
 				<div align="center">
 					<u>Nom de l\'expéditeur :</u>'.$_POST['nom'].'<br />
+					<u>Prénom de l\'expéditeur :</u>'.$_POST['prenom'].'<br />
+					<u>Société de l\'expéditeur :</u>'.$_POST['prenom'].'<br />
+					<u>Numéro de téléphone de l\'expéditeur :</u>'.$_POST['prenom'].'<br />
 					<u>Mail de l\'expéditeur :</u>'.$_POST['mail'].'<br />
+					<u>
 					<br />
 					'.nl2br($_POST['message']).'
 				</div>
@@ -23,9 +25,7 @@ if(isset($_POST['envoyer']))
 
 		mail("stagiaire@socafluid.fr", "CONTACT - Groupe-Socafluid.fr", $message, $header);
 		$msg="Votre message a bien été envoyé !";
-	}
-	else
-	{
+	} else {
 		$msg="Tous les champs doivent être complétés !";
 	}
 }
